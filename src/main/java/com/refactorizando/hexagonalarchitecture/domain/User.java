60658f184873977cfa623f2be6c137a51da3db35
Christopher Gustavo Sanchez Calderon <csanchezcal@proveedor.intercorp.com.pe>
2021-09-27 16:04:00 -0500

    first commit

---
+++ src/main/java/com/refactorizando/hexagonalarchitecture/domain/User.java
@@ -0,0 +1,24 @@
+package com.refactorizando.hexagonalarchitecture.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter
+@Setter
+@Entity
+public class User {
+
+  @Id
+  @GeneratedValue(strategy= GenerationType.AUTO)
+  private Long id;
+
+  private String name;
+
+  private String address;
+
+}
---
+++ src/main/java/com/refactorizando/hexagonalarchitecture/infrastructure/db/springdata/repository/UserDboRepository.java
@@ -0,0 +1,30 @@
+package com.refactorizando.hexagonalarchitecture.infrastructure.db.springdata.repository;
+
+import org.springframework.stereotype.Service;
+
+import com.refactorizando.hexagonalarchitecture.application.repository.UserRepository;
+import com.refactorizando.hexagonalarchitecture.domain.User;
+import com.refactorizando.hexagonalarchitecture.infrastructure.db.springdata.mapper.UserEntityMapper;
+
+import lombok.RequiredArgsConstructor;
+
+
+@RequiredArgsConstructor
+@Service
+public class UserDboRepository implements UserRepository {
+
+  private final SpringDataUserRepository userRepository;
+
+  private final UserEntityMapper userMapper;
+
+  @Override
+  public User findById(Long id) {
+    return userMapper.toDomain(userRepository.findById(id).orElseThrow());
+  }
+
+  @Override
+  public User save(User user) {
+    return userMapper.toDomain(userRepository.save(userMapper.toDbo(user)));
+  }
+
+}
